632571f3e234f6a3e8b4331e22b16a6175543d31
Kayzels <kzn.hartwig1@gmail.com>
2022-12-13 15:06:43 +0200

    Complete Remove From Array Exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values) {
+    function notEqualsValue(valueToCheck) {
+        for (const value of values) {
+            if (valueToCheck === value) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return array.filter(notEqualsValue);
 };
 
 // Do not edit below this line
