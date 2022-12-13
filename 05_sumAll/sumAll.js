e52698e643e0048b3676701326fcbdf902c66e72
Kayzels <kzn.hartwig1@gmail.com>
2022-12-13 15:19:26 +0200

    Complete Sum All

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    for (const arg of arguments) {
+        if (typeof arg != "number" || arg < 0) {
+            return "ERROR";
+        }
+    }
+    let sum = 0;
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
